5dcafb235cc4310d2620f1552d2ff8ffba39f572
John Doe <johndoe@email.com>
2024-10-18 18:08:05 -0400

    Add event listener to update price on size selection

--- main.js
+++ main.js
@@ -4,3 +4,15 @@
 
 
 //Task 2: Add Event Listeners for Product Selection
+
+//setting priceElement and sizeSelector
+const priceElement = document.getElementById("product-price");
+const sizeSelector = document.getElementById("size-selector");
+
+//creating an event listener for whater size it is changed to
+sizeSelector.addEventListener("change", (event) => {
+    const selectedPrice = event.target.value;
+
+//using textContent to fill in the string message
+    priceElement.textContent = `$${selectedPrice}`
+})

3782d1b472b576fb4c5809d55f099a7df6ef4ea9
John Doe <johndoe@email.com>
2024-10-19 14:22:01 -0400

    Create checkout event and confirmation message

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//was not able to figure out the availability aspect of the assignment
+
 //Task 1: Setup Product HTML Structure
 //Done in index.html file
 
@@ -5,7 +7,7 @@
 
 //Task 2: Add Event Listeners for Product Selection
 
-//setting priceElement and sizeSelector
+//setting priceElement and sizeSelector variables
 const priceElement = document.getElementById("product-price");
 const sizeSelector = document.getElementById("size-selector");
 
@@ -16,3 +18,31 @@ sizeSelector.addEventListener("change", (event) => {
 //using textContent to fill in the string message
     priceElement.textContent = `$${selectedPrice}`
 })
+
+
+
+//Task 3: Handle Stock Availability
+
+//create variables for either In Stock or Out of Stock
+const inStock = document.getElementById("in-stock")
+const outOfStock = document.getElementById("out-of-stock")
+
+//disable purchase button if size is Out of Stock
+//outOfStock.addEventListener("change", ( => {}))
+
+
+//Task 4: Create a Checkout Event
+
+//create variable for purchase
+const purchase = document.getElementById("buy-the-shirt");
+
+//create pop up alert message for both instock and out of stock
+purchase.addEventListener("click", () => {
+    if (document.getElementById("in-stock")) {
+    alert("Success! You have purchased the Button Down Shirt.");
+        } else { 
+            alert("Size Out of Stock.")
+    }
+})
+
+//I couldn't figure out how to fix the if statement above ^
